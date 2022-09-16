ff73fad339b363040a36be9b5b039f782004388f
hiroka1642 <uver77704222@gmail.com>
2022-09-16 18:22:42 +0900

    ✨ UserListを表示する

--- App.tsx
+++ App.tsx
@@ -1,51 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
-import {
-  createNativeStackNavigator,
-  NativeStackScreenProps,
-} from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
-import { Button, View } from "react-native";
-import { FlatListBasics } from "./components/FlatListBasics";
-import { SectionListBasics } from "./components/SectionListBasics";
+import { UserListScreen } from "./screens";
 
 export type RootStackParamList = {
-  Home: undefined;
-  FlatListBasics: undefined;
-  SectionListBasics: undefined;
+  UserListScreen: undefined;
 };
-
-type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
-
-export const HomeScreen = ({ navigation }: Props) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        title="Go to FlatListBasics"
-        onPress={() => navigation.navigate("FlatListBasics")}
-      />
-      <Button
-        title="Go to SectionListBasics"
-        onPress={() => navigation.navigate("SectionListBasics")}
-      />
-    </View>
-  );
-};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <RootStack.Navigator initialRouteName="Home">
+      <RootStack.Navigator initialRouteName="UserListScreen">
         <RootStack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Home" }}
-        />
-        <RootStack.Screen name="FlatListBasics" component={FlatListBasics} />
-        <RootStack.Screen
-          name="SectionListBasics"
-          component={SectionListBasics}
+          name="UserListScreen"
+          component={UserListScreen}
+          options={{ title: "UserList" }}
         />
       </RootStack.Navigator>
     </NavigationContainer>
